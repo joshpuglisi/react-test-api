6b4db391ac4dc4ecfc0e394040622957db98afe1
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2023-04-08 16:30:56 +0200

    added items into dropdown and sorted

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
   const getBerryList = async() => {
     const fetchList = await fetch("https://pokeapi.co/api/v2/berry/"); //REST API call, need to await first because they work asynchronous
     const readList = await fetchList.json(); //json function to read the fetched API as json. Await is needed, otherwise the promise will be pending.
-    console.log(readList);
+    const listMap = readList.results.map(data => {
+      return {
+        value : data.name,
+        label : data.name
+      }
+    })
+    setBerryList(listMap.sort((a,b) => a.value.localeCompare(b.value))); //input listMap into berryList. And then berryList is referenced in HTML. The sort function is to compare first and second value, the rest is auto
   }
 
   useEffect(() => {
@@ -18,7 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      {/*<Select options={options} placeholder="Choose your berries" id='berry-drop'></Select>*/}
+      <Select options={berryList} placeholder="Choose your berries" id='berry-drop'></Select>
     </div>
   );
 }
