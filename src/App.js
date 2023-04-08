072c735dd59a819ae7ac6a831b33e06ac11152ca
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2023-04-08 17:32:23 +0200

    select done, option can be seen, training #1 done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Select from 'react-select'
 
 function App() {
 
-  const [berryList, setBerryList] = useState()
+  const [berryList, setBerryList] = useState();
+  const [outBerry, setOutBerry] = useState("");
 
   const getBerryList = async() => {
     const fetchList = await fetch("https://pokeapi.co/api/v2/berry/"); //REST API call, need to await first because they work asynchronous
     const readList = await fetchList.json(); //json function to read the fetched API as json. Await is needed, otherwise the promise will be pending.
-    const listMap = readList.results.map(data => {
+    const listMap = readList.results.map(data => { //map function is used to map the json into new items array for SELECT.
       return {
-        value : data.name,
-        label : data.name
+        value : data.name, //the value field will be assigned with name from json data
+        label : data.name //the label field will be assigned with also name from json data
       }
     })
     setBerryList(listMap.sort((a,b) => a.value.localeCompare(b.value))); //input listMap into berryList. And then berryList is referenced in HTML. The sort function is to compare first and second value, the rest is auto
+  }
+
+  const handleChange = (value) => {
+      setOutBerry(value);
   }
 
   useEffect(() => {
@@ -24,7 +29,10 @@ function App() {
 
   return (
     <div className="App">
-      <Select options={berryList} placeholder="Choose your berries" id='berry-drop'></Select>
+      <h2>The Berry you have selected is:</h2>
+      <h1>{outBerry}</h1>
+      <Select options={berryList} placeholder="Choose your berries" id='berry-drop' onChange={(e) => handleChange(e.value)}></Select>
+      {/*<Select options={berryList} placeholder="Choose your berries" id='berry-drop' onChange={(e) => console.log('event', e)}></Select> ---> used to record output for precaution*/}
     </div>
   );
 }
